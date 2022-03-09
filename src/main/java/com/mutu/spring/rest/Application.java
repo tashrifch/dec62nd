e47c37634e34cb25c412e347151c8014fbecd310
justinagain <jmdv@buffalo.edu>
2022-03-08 21:01:09 -0500

    Update Application.java

--- src/main/java/com/mutu/spring/rest/Application.java
+++ src/main/java/com/mutu/spring/rest/Application.java
@@ -11,6 +11,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+	
+	public static String KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
